1855f3d275cb23698af3cd71d6633b84482b2db7
sinyovercosy <yan.tangent@gmail.com>
2017-07-10 12:35:51 -0500

    extract feats from chb-mit data

--- haha.m
+++ haha.m
@@ -1,23 +1,28 @@
-Fs = 173.61;
-window_size_in_sec = 4097/Fs;
-step_size_in_sec = 4097/Fs;
-classes = {'healthy','healthy','interictal','interictal','ictal'};
+Fs = 256;
+window_size_in_sec = 1;
+step_size_in_sec = 1;
+%classes = {'healthy','healthy','interictal','interictal','ictal'};
 headers = {'variance','skewness','kurtosis','energy','entropy',...
     'spectral_centroid','spectral_spread','spectral_entropy','spectral_flux','spectral_rolloff'};
-features = cell(5,6);
-for set=1:5
-    class = cell(100,1);
-    class(:) = classes(set);
-    class=categorical(class);
-    for imf=1:5
-        for epoch=1:100
-            features{set,imf} = [features{set,imf}, feats(IMFs{set,epoch}(imf,:)',Fs,window_size_in_sec,step_size_in_sec)];
-        end;
-        %features{set,imf} = num2cell(features{set,imf});
-        %[features{set,imf}{11,:}] = deal(classes{set});
-        features{set,imf} = array2table(features{set,imf}','VariableNames',strcat(headers,sprintf('_imf%d',imf)));
-        features{set,6} = [features{set,6},features{set,imf}];
-        features{set,imf}.class = class;
-    end;
-    features{set,6}.class = class;
-end;
+features = cell(23,7);
+
+for chan=1:23
+
+    for seizure=1:size(seizures,2)
+        features{chan,seizure} = cell(6,1);
+        for imf=1:5
+            features{chan,seizure}{imf} = feats(cat(2,seizures{chan,seizure}{imf,:})',Fs,window_size_in_sec,step_size_in_sec);
+            features{chan,seizure}{imf} = array2table(features{chan,seizure}{imf}','VariableNames',strcat(headers,sprintf('_imf%d',imf)));
+            features{chan,seizure}{6} = [features{chan,seizure}{6},features{chan,seizure}{imf}];
+            class = cell(size(features{chan,seizure}{imf},1),1);
+            class(:) = {'ictal'};
+            class=categorical(class);
+            features{chan,seizure}{imf}.class = class;
+        end
+        class = cell(size(features{chan,seizure}{6},1),1);
+        class(:) = {'ictal'};
+        class=categorical(class);
+        features{chan,seizure}{6}.class = class;
+    end
+    
+end
